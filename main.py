b1db4dc9bfe274e453b6d0b5907fb5ba34154d87
unknown <alexanisandr@gmail.com>
2023-01-25 08:34:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from config import *
+from flask import Flask, redirect, url_for, render_template
+
+server = Flask(__name__)
+
+@server.route('/')
+def home():
+    return redirect(url_for('homepage'))
+
+@server.route('/home', methods=['GET', 'POST'])
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
